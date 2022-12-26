ab9cce153f9be26b8b21f068a3f26e8f6637aa14
paungmaio <paungmaio@163.com>
2022-12-26 16:05:24 +0800

    测试百度规则

--- rewrite/api-baidu-map.js
+++ rewrite/api-baidu-map.js
@@ -1,5 +1,5 @@
 const body = JSON.parse($response.body)
 console.log(body)
-body.result.location.lng=104.091112
-body.result.location.lat=30.5955555
+body.result.location.lng=6666666666
+body.result.location.lat=6666666666666
 $done({body:JSON.stringify(body)});
