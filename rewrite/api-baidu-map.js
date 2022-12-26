e95bcce6231048a33aa0ffb9fcb60c47737cbb3a
paungmaio <paungmaio@163.com>
2022-12-26 14:36:32 +0800

    测试百度规则

--- rewrite/api-baidu-map.js
+++ rewrite/api-baidu-map.js
@@ -1,4 +1,5 @@
-const body = JSON.parse($response.body)
-body.result.lng=104.091112
-body.result.lat=30.5955555
-$done({body:JSON.stringify(body)});
+// const body = JSON.parse($response.body)
+console.log($response)
+// body.result.lng=104.091112
+// body.result.lat=30.5955555
+$done();
