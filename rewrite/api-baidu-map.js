8fe384d8ae52b557789f8ce7748979ad66e898d6
paungmiao <49259998+paungmiao@users.noreply.github.com>
2022-12-26 16:22:47 +0800

    Update api-baidu-map.js

--- rewrite/api-baidu-map.js
+++ rewrite/api-baidu-map.js
@@ -1,5 +1,5 @@
 const body = JSON.parse($response.body)
-console.log(body)
+console.log('api-map-console',body)
 body.content.point.x="100.000000"
 body.content.point.y="30.000000"
 $done({body:JSON.stringify(body)});
