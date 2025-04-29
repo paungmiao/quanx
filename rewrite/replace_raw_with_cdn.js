a5d166429e234b2a4325fa3b722eb1f0851cd36d
paungmiao <49259998+paungmiao@users.noreply.github.com>
2025-04-29 10:48:25 +0800

    Update replace_raw_with_cdn.js

--- rewrite/replace_raw_with_cdn.js
+++ rewrite/replace_raw_with_cdn.js
@@ -1,4 +1,5 @@
 // Quantumult X script-response-body
+// 测试返回：https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/yj.js
 let body = $response.body;
 if (body) {
   body = body.replace(/https:\/\/raw\.githubusercontent\.com\/([^\/]+)\/([^\/]+)\/([^\/]+)\/([^\s"']+)/g, 'https://cdn.jsdelivr.net/gh/$1/$2@$3/$4');
