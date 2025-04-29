b90edbd2a4fb80cab87fafc8f23a26bc66e2b426
paungmiao <49259998+paungmiao@users.noreply.github.com>
2025-04-29 10:49:04 +0800

    Update replace_raw_with_cdn.js

--- rewrite/replace_raw_with_cdn.js
+++ rewrite/replace_raw_with_cdn.js
@@ -2,6 +2,7 @@
 // 测试返回：https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/yj.js
 let body = $response.body;
 if (body) {
+  log.info('https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/yj.js')
   body = body.replace(/https:\/\/raw\.githubusercontent\.com\/([^\/]+)\/([^\/]+)\/([^\/]+)\/([^\s"']+)/g, 'https://cdn.jsdelivr.net/gh/$1/$2@$3/$4');
 }
 $done({ body });
