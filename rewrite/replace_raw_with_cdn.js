92653a789585db4a21b91a307737e2cc1dfd27cc
paungmiao <49259998+paungmiao@users.noreply.github.com>
2025-04-29 10:50:44 +0800

    Update replace_raw_with_cdn.js

--- rewrite/replace_raw_with_cdn.js
+++ rewrite/replace_raw_with_cdn.js
@@ -1,8 +1,6 @@
-// Quantumult X script-response-body
-// 测试返回：https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/yj.js
+// Quantumult X script-response-body 测试返回：https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/yj.js
 let body = $response.body;
 if (body) {
-  log.info('https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/yj.js')
   body = body.replace(/https:\/\/raw\.githubusercontent\.com\/([^\/]+)\/([^\/]+)\/([^\/]+)\/([^\s"']+)/g, 'https://cdn.jsdelivr.net/gh/$1/$2@$3/$4');
 }
 $done({ body });
